feb374664b0aea53703f98d8006d3f6bfd89d834
Kevin Ku <kku@yahoo-inc.com>
2014-06-06 15:51:07 -0700

    fixed typo in custom options in runner

--- cronshot-runner.js
+++ cronshot-runner.js
@@ -1,8 +1,11 @@
 var cronshot = require('./cronshot');
 
+// capture with default options
 cronshot.startCapturing(null);
+
+// capture with custom options
 cronshot.startCapturing({
   url: 'http://www.yahoo.com',
   imageName: 'yahoo.png',
-  mobstorePath: 'yahoo.png' 
+  mobstorPath: 'yahoo.png' 
 });
