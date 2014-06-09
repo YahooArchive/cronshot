9f455ddb0cc8919b7a27210e9543c2c8259d4563
Greg Franko <gregfranko5@gmail.com>
2014-06-09 13:46:29 -0700

    Updated cronshot-runner.js to use the default options

--- cronshot-runner.js
+++ cronshot-runner.js
@@ -1,10 +1,10 @@
 var cronshot = require('./cronshot');
 
 // capture with default options
-// cronshot.startCapturing();
+cronshot.startCapturing();
 
 // capture with custom options
-cronshot.startCapturing({
-  // url: 'http://www.yahoo.com',
-  customCSS: '.js-applet-view-container-main { background: red !important; }'
-});
+// cronshot.startCapturing({
+//   url: 'http://www.yahoo.com',
+//   customCSS: '.js-applet-view-container-main { background: red !important; }'
+// });
